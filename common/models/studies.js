8b3404883bd1ced6541a21131683ea95df8cb23d
aymanm <ayman.yousif@gmail.com>
2017-02-13 17:11:08 +0400

    inital commit

---
+++ common/models/studies.js
@@ -0,0 +1,8 @@
+'use strict';
+//var BriefSummaries = require('./brief-summaries')
+module.exports = function(Studies) {
+  //  Studies.embedsMany(BriefSummaries, {
+  //      as: 'briefSummaries', // default to the relation name - emailAddresses
+  //      property: 'briefSummaries' // default to emailAddressItems
+  //      });
+};
