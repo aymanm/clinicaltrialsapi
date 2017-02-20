00ebdca137eb2539850eb1ce9dbf9d0929b6f547
aymanm <ayman.yousif@gmail.com>
2017-02-20 23:55:11 +0400

    added relations and queries

--- common/models/studies.js
+++ common/models/studies.js
@@ -1,8 +1,29 @@
 'use strict';
 //var BriefSummaries = require('./brief-summaries')
 module.exports = function(Studies) {
-  //  Studies.embedsMany(BriefSummaries, {
-  //      as: 'briefSummaries', // default to the relation name - emailAddresses
-  //      property: 'briefSummaries' // default to emailAddressItems
-  //      });
+  
+  Studies.Search = function (text, cb) {
+
+        var ds = Studies.dataSource;
+        var sql = "SELECT * FROM Studies WHERE brief_title ilike '%' || $1 || '%'";
+
+        ds.connector.query(sql, [text], function (err, studies) {
+
+            if (err) console.error(err);
+
+            cb(err, studies);
+
+        });
+
+    };
+
+    Studies.remoteMethod(
+        'Search',
+        {
+            http: { verb: 'get' },
+            description: 'Get search studies',
+            accepts: { arg: 'text', type: 'string' },
+            returns: { arg: 'data', type: ['Studies'], http: {path: "/:text"} }
+        }
+    );
 };
